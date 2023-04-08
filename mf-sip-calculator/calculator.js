d258e64a87ea18becb0e695397641695649d7e0e
Satish Kumbhar <40035899+sats17@users.noreply.github.com>
2023-04-08 13:50:46 +0530

    Update calculator.js

--- mf-sip-calculator/calculator.js
+++ mf-sip-calculator/calculator.js
@@ -1,10 +1,20 @@
 function sipCalculator(expenseRatio, monthlyInvestment, returnsPercentage, inflationRate, years) {
   const monthlyRateOfReturn = (1 + returnsPercentage / 100) ** (1 / 12) - 1;
+  const dailyExpenseRatio = expenseRatio / 100 / 365;
   const n = years * 12;
-  const futureValue = monthlyInvestment * (((1 + monthlyRateOfReturn) ** n - 1) / monthlyRateOfReturn);
+  let futureValue = 0;
+  
+  for (let i = 0; i < n; i++) {
+    let currentMonthInvestmentValue = monthlyInvestment;
+    for (let j = 0; j < 30; j++) {
+      currentMonthInvestmentValue *= (1 - dailyExpenseRatio);
+    }
+    futureValue *= (1 + monthlyRateOfReturn);
+    futureValue += currentMonthInvestmentValue;
+  }
+  
   const totalInvestment = monthlyInvestment * n;
-  const expenseAmount = totalInvestment * expenseRatio / 100;
-  const realReturns = futureValue - totalInvestment - expenseAmount;
-  const realReturnsAfterInflation = realReturns / ((1 + inflationRate / 100) ** years);
-  return [futureValue, realReturns, realReturnsAfterInflation];
+  const expenseAmount = totalInvestment - (futureValue / (((1 + monthlyRateOfReturn) ** n - 1) / monthlyRateOfReturn) * monthlyInvestment);
+  const futureValueAfterInflation = futureValue / ((1 + inflationRate / 100) ** years);
+  return [futureValue, expenseAmount, futureValueAfterInflation];
 }
